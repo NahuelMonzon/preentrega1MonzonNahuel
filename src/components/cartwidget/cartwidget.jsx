f96170e45ab4ec26d704fa103618c18cf0694201
Nahuel Monzon <nahusprint@gmail.com>
2024-05-22 18:51:36 -0300

    first commit

---
+++ src/components/cartwidget/cartwidget.jsx
@@ -0,0 +1,9 @@
+import react from `react`
+
+const Cartwidget = {} => {
+    return (
+        <div>Cartwidget</div>
+    )
+}
+
+export default Cartwidget
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from 'antd';
+import 'antd/dist/antd.css';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <Button>Home</Button>
+    </nav>
+  );
+};
+
+export default Navbar;
